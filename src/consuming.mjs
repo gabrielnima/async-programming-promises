25e8b8f05190b09b705f10fd3913c00e54628f91
Gabriel Nima <gabrielnima@ravn.co>
2022-04-11 16:41:00 -0500

    resolve consuming

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,16 +1,65 @@
-import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
+import setText, { appendText, showWaiting, hideWaiting } from "./results.mjs";
 
 export function get() {
+  //destruccturación
+  axios.get("http://localhost:3000/orders/1").then(({ data }) => {
+    setText(JSON.stringify(data));
+  });
 }
 
 export function getCatch() {
+  axios
+    .get("http://localhost:3000/orders/123")
+    .then(({ data }) => {
+      setText(JSON.stringify(data));
+    })
+    .catch((error) => setText(error));
 }
 
 export function chain() {
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+      return axios.get(
+        `http://localhost:3000/addresses/${data.shippingAddress}`
+      );
+    })
+    .then(({ data }) => setText(data.city));
 }
 
 export function chainCatch() {
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+      axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+      throw new Error("!Error");
+    })
+    .catch((error) => {
+      setText(error);
+      throw new Error("Error 2");
+    })
+    .then(({ data }) => setText(data.city))
+    .catch((error) => setText(error));
 }
 
 export function final() {
+  showWaiting();
+  axios
+    .get("http://localhost:3000/orders/1")
+    .then(({ data }) => {
+      axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+      throw new Error("!Error");
+    })
+    .catch((error) => {
+      setText(error);
+      throw new Error("Error 2");
+    })
+    .then(({ data }) => setText(data.city))
+    .catch((error) => setText(error))
+    .finally(() => {
+      setTimeout(() => {
+        hideWaiting();
+      }, 1500);
+      appendText("--Completely Done");
+    });
 }
